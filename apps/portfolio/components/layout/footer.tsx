ad3f79f40ff6644e7f4a38af08df639bc4c876e9
Andrew Wang <awwang3@uci.edu>
2026-05-08 15:56:47 -0700

    fix: styling

--- apps/portfolio/components/layout/footer.tsx
+++ apps/portfolio/components/layout/footer.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { IslandIcon } from "@phosphor-icons/react/ssr";
 import React from "react";
+
+import { cn } from "@/lib/util/cn";
 
 export function Footer() {
   return (
-    <footer className="mx-auto flex max-w-6xl justify-end px-6 py-2">
-      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-      <a className="text-muted-foreground/75" href="/">
-        Andrew Wang
-      </a>
+    <footer
+      className={cn(
+        "mx-auto flex max-w-6xl items-center justify-end px-6 py-2",
+        "text-muted-foreground/75 space-x-2",
+      )}
+    >
+      <IslandIcon size={20} />
+      <span>Andrew Wang</span>
     </footer>
   );
 }
